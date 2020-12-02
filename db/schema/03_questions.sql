6ae43a65ca942c248158203c284d1ef8a5a9e164
deonleer <deon.leer96@gmail.com>
2020-12-02 19:53:48 +0000

    fixed questions schema

--- db/schema/03_questions.sql
+++ db/schema/03_questions.sql
@@ -2,5 +2,10 @@ DROP TABLE IF EXISTS questions CASCADE;
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id),
-  question VARCHAR(255)
+  question VARCHAR(255),
+  answerA VARCHAR(255),
+  answerB VARCHAR(255),
+  answerC VARCHAR(255),
+  answerD VARCHAR(255),
+  correct_answer VARCHAR(255)
 );
--- db/seeds/05_attempts.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO quiz_attempts (user_id, quiz_id, url)
-VALUES (
-  2, 1, 'attempt1'
-);
