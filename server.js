1decc18144f7a8787d7bb1daf4c5b00dbecdee4f
deonleer <deon.leer96@gmail.com>
2020-12-03 01:23:42 +0000

    added quizattempt route to server router

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const quizzesRoutes = require("./routes/quizzes");
 const newquizRoutes = require("./routes/newquiz");
 const resultsRoutes = require("./routes/results");
 const newquestionRoutes = require("./routes/newquestion");
+const quizAttemptRoutes = require("./routes/quizAttempt");
 
 const { user } = require('pg/lib/defaults');
 // Mount all resource routes
@@ -48,8 +49,9 @@ app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use ("/", quizzesRoutes(db));
 app.use ("/", newquizRoutes(db));
-app.use ("/", resultsRoutes(db))
-app.use ("/", newquestionRoutes(db))
+app.use ("/", resultsRoutes(db));
+app.use ("/", newquestionRoutes(db));
+app.use ("/", quizAttemptRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
