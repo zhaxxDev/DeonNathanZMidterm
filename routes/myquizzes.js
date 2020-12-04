96b7aacda6c9820b1af2b185cb7c1f00d3534821
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-12-03 17:50:37 -0800

    myquizzes

--- routes/myquizzes.js
+++ routes/myquizzes.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-   router.get("/myquizzes", (req, res)=> {
-    const templateVars = {
-      username: req.cookies["username"],
-    };
-    res.render("myquizzes", templateVars);
-  })
+  router.get("/myquizzes",(req, res) => {
+    const id = req.cookies["id"];
+    db.query(`SELECT name, url
+    FROM quizzes
+    WHERE user_id = $1
+    ;`, [id])
+    .then(data => {
+      const quizzes = data.rows;
+      let templateVars = {quizzes: quizzes, username: req.cookies["username"], id}
+      res.render("quizzes", templateVars);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
   return router;
 };
